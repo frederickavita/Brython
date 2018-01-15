195be20f97ececc6e2c4889ef4cda85cbb1de323
frederic <amihaconsulting@gmail.com>
2018-01-15 15:42:22 +0100

    update table.py

--- py/table.py
+++ py/table.py
@@ -8,6 +8,9 @@ product = [["pint of milk", 0.19, 7],
            ['orange', 0.25, 2],
            ['juice', 0.60, 1]]
 
+
+
+
 item = product[0][0]
 price = product[0][1]
 quantity = product[0][2]
@@ -29,29 +32,44 @@ def formate(num, quant):
 
 app_tmpl = Template(doc["container-1"])
 app_tmpl.render(item=item, price=price,
-                quantity=quantity,
+                quantity=quantity, product=product,
                 total=formate(price, quantity))
 
 
 def update_value(ev, elt):
+    """
+      update new value
+      """
     global product
-    """
-    update new value
-    """
-    prod = random.choice(product)
-    app_tmpl.render(item=prod[0],
-                    price=prod[1],
-                    quantity=prod[2],
-                    total=formate(prod[1], prod[2]))
+    if ev.target.value == "banane":
+        item = product[1][0]
+        price = product[1][1]
+        quantity = product[1][2]
+    elif ev.target.value == "orange":
+        item = product[2][0]
+        price = product[2][1]
+        quantity = product[2][2]
+    elif ev.target.value == "juice":
+        item = product[3][0]
+        price = product[3][1]
+        quantity = product[3][2]
+    else:
+        item = product[0][0]
+        price = product[0][1]
+        quantity = product[0][2]
+    print(ev.target.value)
+    app_tmpl.render(item=item,
+                    price=price,
+                    quantity=quantity,
+                    total=formate(price,quantity))
     doc['container-1'].style.display = "block"
     doc['container-2'].style.display = "block"""
 
 
-Template(doc["container-2"], [update_value]).render(item=item,
-                                                    price=price,
-                                                    quantity=quantity,
-                                                    update="update random",
-                                                    total=formate(price, quantity))
+Template(doc["container-2"],[update_value]).render(item=item, price=price,
+                                    quantity=quantity,update="update random",
+                                    product=product,
+                                    total=formate(price, quantity))
 
 
 def loading():
